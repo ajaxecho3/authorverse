a7a155b1c56e4067057b21a97be03de0b93d875f
Bernardino Ochoa <bernardo.ochoa@concentrix.com>
2024-04-12 18:55:13 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { ThemeProvider } from "@/hooks/ThemeProvider";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "AuthorVerse",
+  description: "The place where authors and readers meet",
 };
 
 export default function RootLayout({
@@ -18,10 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+      <body className="text-black dark:bg-gray-900 dark:text-white ">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main className="min-h-screen flex flex-col items-center">
+            {children}
+          </main>
+          <ThemeSwitcher className="fixed bottom-4 right-4" />
+        </ThemeProvider>
       </body>
     </html>
   );
